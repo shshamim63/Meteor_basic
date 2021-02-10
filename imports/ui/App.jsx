3e38acaaa32cfc63e989b8d98930d62fbf0a3d54
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2021-02-10 10:31:48 +0600

    Added methods and scopes to collect data from database and pass them in child component as prop

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Task } from './Task';
 import { useTracker } from 'meteor/react-meteor-data';
-import { TasksCollection } from "/imports/api/TasksCollection.js";
+import { TasksCollection } from '/imports/api/TasksCollection.js';
+import { TaskForm } from './TaskForm';
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
- 
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const pendingTasksCount = useTracker(() => 
+    TasksCollection.find(hideCompletedFilter).count()
+  );
+
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ''
+  }`;
+
+  const tasks = useTracker(() =>
+    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch()
+  );
+
+  const toggleChecked = ({ _id, isChecked }) => {
+    TasksCollection.update(_id, {
+      $set: {
+        isChecked: !isChecked
+      }
+    })
+  };
+
+  const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
   return (
     <div>
-      <h1>Welcome to Meteor!</h1>
- 
+      <h1>
+        📝️ To Do List
+        {pendingTasksTitle}
+      </h1>
+      <TaskForm/>
+      <div className="filter">
+         <button onClick={() => setHideCompleted(!hideCompleted)}>
+           {hideCompleted ? 'Show All' : 'Hide Completed'}
+         </button>
+       </div>
       <ul>
-        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+        { tasks.map(task => <Task
+          key={ task._id }
+          task={ task }
+          onCheckboxClick={toggleChecked} 
+          onDeleteClick={deleteTask} 
+          />)
+        }
       </ul>
     </div>
   );
